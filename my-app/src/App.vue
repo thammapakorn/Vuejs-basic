368d6e153905d381b27edf9694c1a3c3e6572175
Thammapakorn <121923286+thammapakorn@users.noreply.github.com>
2025-01-28 21:30:10 +0700

    สร้าวตัวแปร เรียกใช้ตัวแปร ใน template

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,6 +1,9 @@
 <template>
  <section>
-    <h1>Pream</h1>
+    <h1>ชื่อผู้สมัครงาน : {{firstName }}</h1>
+    <h1>นามสกุล : {{lastName }}</h1>
+    <h2>{{ 8700+200 }}</h2>
+    <h2>ที่อยู่ : <span v-html="address"></span></h2>
  </section>
 </template>
 
@@ -8,16 +11,22 @@
 
 export default {
   name: 'App',
+  data(){
+    return{
+      firstName: "Pream1100",
+      lastName: "Nilpan",
+      age: 15,
+      address:"<i>Bangkok</i>"
+    }
+  },
+  methods:{
+    getFullName(){
+      return this.firstName + this.lastName
+    }
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
